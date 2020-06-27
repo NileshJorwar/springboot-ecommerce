590216600bbad14233d9912a9f39f4a0b64139ca
NileshJorwar <jorwarnilesh@gmail.com>
2020-06-26 20:50:38 -0500

    Initial commit

---
+++ src/main/java/com/nilesh/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,14 @@
+package com.nilesh.ecommerce.dao;
+
+import com.nilesh.ecommerce.entity.Product;
+import com.nilesh.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+// productCategory below is Json Entry and path = product-category is path /product-category
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
