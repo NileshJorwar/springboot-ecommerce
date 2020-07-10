3cc9893aeb249cb242ac3b1dbaa274c4a687ecec
NileshJorwar <jorwarnilesh@gmail.com>
2020-07-10 12:53:18 -0500

    country and states added , needed by angular app for drop down

---
+++ src/main/java/com/nilesh/ecommerce/entity/Country.java
@@ -0,0 +1,36 @@
+package com.nilesh.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+}
